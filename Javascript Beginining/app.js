cdee68d7e9c8069d4731be0942f30563ec28f57a
AbdulkadirCELEBI02 <abdlkadirclb@gmail.com>
2021-02-19 16:20:42 +0300

    lesson

---
+++ Javascript Beginining/app.js
@@ -0,0 +1,135 @@
+// let value;
+
+// const value1 = 10;
+
+// const value2 = 20;
+
+// //Aritmetik oparatörler
+
+// value = (value1 + value2);
+
+
+// value = Math.PI;
+
+
+// console.log(value);
+
+
+// const firstiName = "Louis";
+// const lastName = "Armstrong";
+
+// const langs = "Java, Python, c++";
+
+// value = firstiName + lastName;
+
+// value = firstiName + " " + lastName;
+
+// value += " Çelebi";
+
+// value = firstiName.length;
+
+// console.log(value);
+
+ const name = "Abdulkadir Çelebi";
+ const department = "Engineer";
+ const salary = 50000;
+
+// const a = `İsim:${name}\nDepartmen:${department}\nMaaş:${salary}`;
+
+const html = 
+    `
+    <u1>
+    <li>${name}</li>
+    <li>${department}</li>
+    <li>${salary}</li>
+
+    </u1>
+
+`
+// document.body.innerHTML = html;
+
+// const number =[1,2,33,523,6];
+
+// value = number;
+
+// value = number.sort(function(x,y){//kü.ükten büyüğe doğru sıralama
+//     return x - y;
+// });
+
+// value = number.sort(function(x,y){
+//     return y - x;//büyükten küçüğe doğru sıralama
+// })
+
+
+// console.log(value);
+
+// let value;
+
+// const programmer = {//obje oluşturma 
+//     name :"Abdulkadir çelebi",
+//     age : 25,
+//     email:"övxcvmcxv",
+//     langs : ["oythın","java","javascript"],
+
+//     addres : {
+//         city : "Ankara",
+//         street : "Bahçelievler"
+
+//     },
+
+//     work : function(){
+//         console.log("Programcı çalışouyor");
+//     }
+// }
+
+// value = programmer;
+
+// value = programmer.email;
+
+// value = programmer.age;
+
+// value = programmer.addres.city;
+
+// programmer.work();
+// programmer.name;
+// console.log(name);
+
+// const programmers = [
+//     {name: "Abdulkadir",age:22},
+//     {name : "Nida",age:21},
+// ];
+
+// value = programmers[0].name;
+
+// console.log(value);
+
+let value ;
+
+const now = new Date();//şuan ki zamanı saati almamzıı sağlar
+
+const date1 = new Date("02-06-1998 00:00:00");
+
+const date2 = new Date("jun 02 1998");
+
+const date3 = new Date("02/06/1998");
+
+value = date1;
+
+value = date1.getMonth();
+value = date1.getDate();
+value = date1.getDate();
+value = date1.getHours();
+value = date1.getMinutes();
+value = date1.getSeconds();
+
+date1.setMonth(7);//set ile istediğimiz şeyleri değiştirmek için kullanılır.
+
+console.log(value);
+
+
+
+
+
+
+
+
